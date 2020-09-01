7af5ae0ae09c59376ce3673447a24104c285b610
Wellisson Nunes <wellissonln@gmail.com>
2020-09-01 19:53:44 -0300

    testando comando do sistema de templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = ['Django', 'Python', 'Git', 'Html',
+    'DB', 'Linux', 'Nginx', 'Uwsgi',
+    'Systemctl'
+    ]
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
