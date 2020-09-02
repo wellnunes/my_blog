1df3cbb4a0b7ddc8b88112064b6f18d155ef6f23
Wellisson Nunes <wellissonln@gmail.com>
2020-09-01 21:17:05 -0300

    realizando algumas alterações no django-admin e configurando template

--- blog/website/models.py
+++ blog/website/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Categorias(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
+
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_category_label(self):
+        return self.get_categories_display()
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Html',
     'DB', 'Linux', 'Nginx', 'Uwsgi',
     'Systemctl'
     ]
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+
+    list_posts = Post.objects.filter(deleted=True)
+
+    data = {'name': 'Curso de Django 3',
+     'lista_tecnologias': lista,
+     'posts': list_posts
+    }
+    
+
     return render(request, 'index.html', data)
