2f626a30bbaabaaacdd316fa9b91197bd7579fe0
Wellisson Nunes <wellissonln@gmail.com>
2020-09-04 20:24:43 -0300

    update to deploy

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -86,7 +86,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
